b228f8087120a816f9d1c7c46d9643f23cb9cdf4
aarrgom935 <aarrgom935@g.educaand.es>
2024-05-02 14:08:44 +0200

    -_-

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
@@ -47,7 +53,9 @@ fun MainWindow(
     studentsFile: File?
 ) {
     MaterialTheme {
-        Surface {
+        Surface(
+            color = Color.LightGray
+        ) {
             StudentScreen(
                 fileManager = fileManager,
                 studentsFile = studentsFile
@@ -63,6 +71,7 @@ fun StudentScreen(
 ){
     var newStudent by remember { mutableStateOf("") }
     var studentsList by remember { mutableStateOf<List<String>>(emptyList()) }
+    val scrollState = rememberScrollState()
 
     LaunchedEffect(studentsFile) {
         studentsFile?.let { file ->
@@ -70,44 +79,100 @@ fun StudentScreen(
             studentsList = loadedStudents
         }
     }
-    Row (
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-
-            OutlinedTextField(
-                value = newStudent,
-                onValueChange = { newStudent = it },
-                label = { Text("Nuevo estudiante") },
-                modifier = Modifier.padding(15.dp)
-            )
-
-            Button(
-                onClick = {
-                    if (newStudent.isNotBlank()) {
-                        studentsList = studentsList + newStudent
-                        newStudent = ""
-                    }
-                },
-                modifier = Modifier.padding(15.dp)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text("Añadir estudiante")
-            }
-        }
 
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            studentsList.forEach { student ->
-                Text(student)
+                OutlinedTextField(
+                    value = newStudent,
+                    onValueChange = { newStudent = it },
+                    label = { Text("New student name") },
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .background(Color.White),
+
+                    )
+
+                Button(
+                    onClick = {
+                        if (newStudent.isNotBlank()) {
+                            studentsList = studentsList + newStudent
+                            newStudent = ""
+                        }
+                    },
+                    modifier = Modifier.padding(15.dp)
+                ) {
+                    Text("Add student")
+                }
+            }
+
+
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .border(2.dp, Color.Black)
+                        .padding(10.dp)
+                        .heightIn(max = 400.dp)
+                        .verticalScroll(scrollState)
+
+                ) {
+                    studentsList.forEach { student ->
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            Text(
+                                text = student,
+                                modifier = Modifier
+                                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                                    .fillMaxWidth(0.4f)
+                            )
+
+                            IconButton(
+                                onClick = { studentsList = studentsList.filter { it != student } },
+                            ) {
+                                Icon(Icons.Filled.Delete, contentDescription = "Delete Student")
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    Button(
+                        onClick = { studentsList = emptyList() },
+                        modifier = Modifier.padding(15.dp)
+                    ) {
+                        Text("Clear all")
+                    }
+
+
+
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                        .fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(scrollState)
+                )
+
+
             }
         }
     }
-
 }
 
