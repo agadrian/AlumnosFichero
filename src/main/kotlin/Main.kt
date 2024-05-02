668b9a5c04025fd3af787f65d92a04579f6f127b
agadrian <agadrian@iesrafaelalberti.es>
2024-05-02 17:51:49 +0200

    scrollbar no se ve :(

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -12,6 +13,7 @@ import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.res.loadImageBitmap
 import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import java.io.File
@@ -35,8 +37,6 @@ fun main() {
         }
     }
 }
-
-
 
 
 @Composable
@@ -93,8 +93,6 @@ fun StudentScreen(
                 label = { Text("New student name") },
                 modifier = Modifier
                     .padding(15.dp)
-                    .background(Color.White),
-
                 )
 
             Button(
@@ -111,41 +109,51 @@ fun StudentScreen(
         }
 
 
-
         // COLUMNA LISTA
         Column(
             modifier = Modifier
-                .background(Color.White)
-                .border(2.dp, Color.Black)
-                .padding(10.dp)
-                .heightIn(max = 400.dp)
                 .verticalScroll(scrollState)
-                .background(color = Color.Red)
-
         ) {
+            Box {
+                Text("Students: ${studentsList.size}",
+                    modifier =  Modifier
+                        .background(Color.Red))
 
+                LazyColumn(
+                    modifier = Modifier
+                        .background(Color.White)
+                        .height(300.dp)
+                        .border(2.dp, Color.Black)
+                        .padding(10.dp)
+                        .widthIn(min = 200.dp, max = 350.dp)
+                ) {
 
-            studentsList.forEach { student ->
-                StudentRow(
-                    student = student,
-                    onDelete = {  studentsList = studentsList.filter { it != student }  }
+                    items(studentsList.size) { index ->
+                        StudentRow(
+                            student = studentsList[index],
+                            onDelete = { studentsList = studentsList.filterIndexed { i, _ -> i != index } }
+                        )
+                    }
+                }
+
+                VerticalScrollbar(
+                    modifier = Modifier
+                        .height(300.dp)
+                        .align(Alignment.CenterEnd),
+                    adapter = rememberScrollbarAdapter(
+                        scrollState = scrollState
+                    )
                 )
             }
 
             Button(
                 onClick = { studentsList = emptyList() },
-                modifier = Modifier.padding(15.dp)
+                modifier = Modifier
+                    .padding(15.dp),
             ) {
                 Text("Clear all")
             }
         }
-
-        VerticalScrollbar(
-            modifier = Modifier.align(Alignment.CenterVertically)
-                .heightIn(max = 400.dp),
-            adapter = rememberScrollbarAdapter(scrollState)
-        )
-
 
         Button(
             onClick = {
@@ -155,11 +163,11 @@ fun StudentScreen(
             },
             modifier = Modifier
                 .padding(16.dp)
-
         ) {
             Text("Save changes")
         }
     }
+
 
 }
 
@@ -172,7 +180,7 @@ fun StudentRow(student: String, onDelete: () -> Unit) {
             text = student,
             modifier = Modifier
                 .padding(horizontal = 16.dp, vertical = 8.dp)
-                .widthIn(min = 100.dp) // Ajustar el ancho mínimo
+                .widthIn(min = 100.dp)
         )
 
         IconButton(
